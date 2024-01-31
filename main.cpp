c9ab2fe1f3de38d01dac9c7784a28f08daff607e
MMariasik <251185@edu.p.lodz.pl>
2024-01-31 12:55:16 +0100

    Nowa metoda wypisz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+void wypisz(int a)
+{
+    std::cout << a << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    wypisz(1);
+
     return 0;
 }
